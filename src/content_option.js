e180c4545c81afbffeae8073e4be9407b61f771d
darshanghoghari <darshanghoghari5657@gmail.com>
2025-03-09 13:11:58 +0530

    Protfolio init

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "DG";
+const meta = {
+    title: "Darshan Ghoghari",
+    description: "I’m Darshan Ghoghari Mern Stack _ Full stack devloper,currently working in Surat",
+};
+
+const introdata = {
+    title: "I’m Darshan Ghoghari",
+    animated: {
+        first: "I Am Mern Stack Developer",
+        second: "I Made Template Design",
+        third: "Advanced Development with TypeScript, Next.js, and Angular",
+    },
+    description: "I specialize in developing and maintaining applications using the MERN (MongoDB, Express.js, React, Node.js) and MEAN (MongoDB, Express.js, Angular, Node.js) stacks, along with Next.js and TypeScript for enhanced performance and scalability.",
+    your_img_url: "https://res.cloudinary.com/smsp-image-management/image/upload/portfolio/s3bz2yuvhxana7bim6lq.jpg",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "A skilled MERN and MEAN stack developer, Darshan Ghoghari holds an MSc degree. His proficiency in coding and GitHub ensures the successful delivery of complex web projects.",
+};
+const worktimeline = [{
+    jobtitle: "Internship (6-month)",
+    company: "Soham-tech",
+    where: "Surat",
+    date: "2023-24(Completed)",
+},
+{
+    jobtitle: "As a Full Stack Developer",
+    company: "Settings Infotech",
+    where: "Uttran-surat",
+    date: "2024 (Running)",
+},
+];
+
+const extraWorkTimeLine = [{
+    jobtitle: "Handle Medical Shop as Seller (As a Part Time (3 years of Experience))",
+    where: "Surat",
+    date: "2021 to 2023",
+}]
+
+const skills = [{
+    name: "HTML , CSS ",
+    value: 95,
+},
+{
+    name: "Javascript",
+    value: 90,
+},
+{
+    name: "Typescript",
+    value: 80,
+},
+{
+    name: "React",
+    value: 80,
+},
+{
+    name: "Next , Angular ",
+    value: 75,
+},
+];
+
+const services = [{
+    title: "Web Development",
+    description: "Darshan Ghoghari excels in web development, specializing in MERN and MEAN stack technologies.",
+},
+{
+    title: "Web Design",
+    description: "Darshan Ghoghari focuses on modern web design, ensuring intuitive user experiences and visually appealing interfaces.",
+},
+{
+    title: "Advanced Web Development with TypeScript, Angular, and Next.js",
+    description: "In services related to web development, TypeScript, Angular, and Next.js are included in building robust, scalable, and efficient web applications. These technologies are utilized for enhancing code quality, ensuring type safety, and providing advanced features such as server-side rendering and static site generation.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "darshanghoghari5657@gmail.com",
+    YOUR_FONE: "+91 90814-60039",
+    description: "I specialize in MERN and MEAN stack development, along with Next.js and TypeScript, to create high-performance, scalable applications. Let's collaborate to bring your project to life!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_jipwa6b",
+    YOUR_TEMPLATE_ID: "template_a9b6bje",
+    YOUR_USER_ID: "Jj1lJEGYd02iS5A67",
+};
+
+const socialprofils = {
+    github: "https://github.com/darshanghoghari",
+    facebook: "https://www.facebook.com/darshan.ghoghari.7771?mibextid=ZbWKwL",
+    linkedin: "https://www.linkedin.com/in/darshan-ghoghari-2840322a6/",
+    twitter: "https://x.com/i/flow/login?redirect_after_login=%2Fmrghoghari23",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    extraWorkTimeLine,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
